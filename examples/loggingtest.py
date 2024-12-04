a4cc17583bb62cf888306818ee2193d1f4ba62cf
levi223 <levi2234@hotmail.com>
2024-12-04 02:08:59 +0100

    log post detaching for other log functions added

--- examples/loggingtest.py
+++ examples/loggingtest.py
@@ -3,11 +3,17 @@ import time
 logger = Plog(host = "127.0.0.1")
 
 for i in range(40):
+    logger.INFO(f"Dit is een voorbeeld INFO bericht ")
+    
+    logger.DEBUG(f"Dit is een voorbeeld DEBUG bericht ")
+    
+    logger.INFO(f"Dit is een voorbeeld INFO bericht ")
+    
+    logger.DEBUG(f"Dit is een voorbeeld DEBUG bericht ")
     logger.LOG(f"Dit is een voorbeeld LOG bericht ")
-    # logger.CRITICAL(f"Dit is een voorbeeld CRITICAL bericht ")
-    # logger.ERROR(f"Dit is een voorbeeld ERROR bericht ")
-    # logger.WARNING(f"Dit is een voorbeeld WARNING bericht ")
-    # logger.INFO(f"Dit is een voorbeeld INFO bericht ")
-    # logger.DEBUG(f"Dit is een voorbeeld DEBUG bericht ")
+    logger.CRITICAL(f"Dit is een voorbeeld CRITICAL bericht ")
+    logger.ERROR(f"Dit is een voorbeeld ERROR bericht ")
+    logger.WARNING(f"Dit is een voorbeeld WARNING bericht ")
+
 
     time.sleep(1)
--- src/ProgressPal/weblog.py
+++ src/ProgressPal/weblog.py
@@ -21,7 +21,7 @@ async def update_progress(message, level, timestamp, filename, lineno, host="127
                     if response.status == 200:
                         return
             except aiohttp.ClientError as e:
-                print(f"Attempt {attempt + 1} failed: {e}")
+                pass
             await asyncio.sleep(1)  # Wait before retrying
 
 class Plog:
@@ -29,97 +29,114 @@ class Plog:
         self.port = port
         self.host = host
         self.filename = os.path.basename(inspect.stack()[1].filename)
+        
+        try:
+            self.loop = asyncio.get_event_loop()
+        except RuntimeError:
+            self.loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.loop) # Set the new event loop as the current event loop if there is an issue with the current event loop (e.g., it is closed or when we get an expected RuntimeError such as running it in a thread)
 
     async def _LOG(self, message, lineno=None):
         level = "LOG"
         timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
+        asyncio.create_task(update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port))
 
     def LOG(self, message):
-        
         frame = inspect.stack()[1]
         lineno = frame.lineno
-        asyncio.run(self._LOG(message,lineno=lineno))
+        if self.loop.is_running():
+            asyncio.create_task(self._LOG(message, lineno=lineno))
+        else:
+            self.loop.run_until_complete(self._LOG(message, lineno=lineno))
 
 
     async def _CRITICAL(self, message, lineno=None):
         level = "CRITICAL"
         timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
+        asyncio.create_task(update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port))
         
     def CRITICAL(self, message):
         frame = inspect.stack()[1]
         lineno = frame.lineno
-        asyncio.run(self._CRITICAL(message,lineno=lineno))
-        
+        if self.loop.is_running():
+            asyncio.create_task(self._CRITICAL(message, lineno=lineno))
+        else:
+            self.loop.run_until_complete(self._CRITICAL(message, lineno=lineno))
+
     async def _ERROR(self, message, lineno=None):
         level = "ERROR"
         timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
+        asyncio.create_task(update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port))
         
     def ERROR(self, message):
         frame = inspect.stack()[1]
         lineno = frame.lineno
-        asyncio.run(self._ERROR(message,lineno=lineno))
-        
+        if self.loop.is_running():
+            asyncio.create_task(self._ERROR(message, lineno=lineno))
+        else:
+            self.loop.run_until_complete(self._ERROR(message, lineno=lineno))
+
     async def _WARNING(self, message, lineno=None):
         level = "WARNING"
         timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
+        asyncio.create_task(update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port))
         
     def WARNING(self, message):
         frame = inspect.stack()[1]
         lineno = frame.lineno
-        asyncio.run(self._WARNING(message,lineno=lineno))
-        
+        if self.loop.is_running():
+            asyncio.create_task(self._WARNING(message, lineno=lineno))
+        else:
+            self.loop.run_until_complete(self._WARNING(message, lineno=lineno))
+
     async def _INFO(self, message, lineno=None):
         level = "INFO"
         timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
+        asyncio.create_task(update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port))
         
     def INFO(self, message):
         frame = inspect.stack()[1]
         lineno = frame.lineno
-        asyncio.run(self._INFO(message,lineno=lineno))
-        
+        if self.loop.is_running():
+            asyncio.create_task(self._INFO(message, lineno=lineno))
+        else:
+            self.loop.run_until_complete(self._INFO(message, lineno=lineno))
+
     async def _DEBUG(self, message, lineno=None):
         level = "DEBUG"
         timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
+        asyncio.create_task(update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port))
         
     def DEBUG(self, message):
         frame = inspect.stack()[1]
         lineno = frame.lineno
-        asyncio.run(self._DEBUG(message,lineno=lineno))
-        
-    async def _CRITICAL(self, message, lineno=None):
-        level = "CRITICAL"
-        timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
-        
-    def CRITICAL(self, message):
-        frame = inspect.stack()[1]
-        lineno = frame.lineno
-        asyncio.run(self._CRITICAL(message,lineno=lineno))
-        
+        if self.loop.is_running():
+            asyncio.create_task(self._DEBUG(message, lineno=lineno))
+        else:
+            self.loop.run_until_complete(self._DEBUG(message, lineno=lineno))
+
     async def _FATAL(self, message, lineno=None):
         level = "FATAL"
         timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
+        asyncio.create_task(update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port))
         
     def FATAL(self, message):
         frame = inspect.stack()[1]
         lineno = frame.lineno
-        asyncio.run(self._FATAL(message,lineno=lineno))
-        
-    
+        if self.loop.is_running():
+            asyncio.create_task(self._FATAL(message, lineno=lineno))
+        else:
+            self.loop.run_until_complete(self._FATAL(message, lineno=lineno))
+
     async def _EXCEPTION(self, message, lineno=None):
         level = "EXCEPTION"
         timestamp = time.ctime()
-        await update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port)
+        asyncio.create_task(update_progress(message, level, timestamp, self.filename, lineno, self.host, self.port))
         
     def EXCEPTION(self, message):
         frame = inspect.stack()[1]
         lineno = frame.lineno
-        asyncio.run(self._EXCEPTION(message,lineno=lineno))
-    
+        if self.loop.is_running():
+            asyncio.create_task(self._EXCEPTION(message, lineno=lineno))
+        else:
+            self.loop.run_until_complete(self._EXCEPTION(message, lineno=lineno))
