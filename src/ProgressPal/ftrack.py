a8b1afd2a6b5aa0f5cec6f789c6433c6b14e87e9
levi223 <levi2234@hotmail.com>
2024-12-06 01:01:30 +0100

    Exchanged asyncio and aiohttp for threads and requests to make frack threadsafe. Additionally added another way to track the same function differently with different identifiers.

--- src/ProgressPal/ftrack.py
+++ src/ProgressPal/ftrack.py
@@ -1,29 +1,31 @@
-
 import time
 from collections import deque
 from functools import wraps
 import os
 import inspect
-import asyncio
-import aiohttp
+import threading
+import requests
+import pickle
 
-async def update_function_progress(task_id, category, call_count, last_execution_time, function_name, exec_hist, error_count, filename, calls_per_second, host, port):
+def update_function_progress(task_id, category, call_count, last_execution_time, function_name, exec_hist, error_count, filename, calls_per_second, host, port):
     """
-    Asynchronously updates the progress of a function execution on a remote server.
-    Args:
-        task_id (str): The unique identifier of the task.
-        category (str): The category of the task.
-        call_count (int): The number of times the function has been called.
-        last_execution_time (float): The time taken for the last execution of the function.
-        function_name (str): The name of the function being tracked.
-        exec_hist (list): The execution history of the function.
-        error_count (int): The number of errors encountered during execution.
-        filename (str): The name of the file where the function is defined.
-        calls_per_second (float): The rate of function calls per second.
-        host (str): The host address of the remote server.
-        port (int): The port number of the remote server.
+    Updates the progress of a function by sending a POST request to a specified server.
+
+    Parameters:
+    task_id (str): The unique identifier for the task.
+    category (str): The category of the task.
+    call_count (int): The number of times the function has been called.
+    last_execution_time (float): The time taken for the last execution of the function.
+    function_name (str): The name of the function being tracked.
+    exec_hist (list): The execution history of the function.
+    error_count (int): The number of errors encountered during execution.
+    filename (str): The name of the file where the function is located.
+    calls_per_second (float): The rate at which the function is being called.
+    host (str): The hostname of the server to send the update to.
+    port (int): The port number of the server to send the update to.
+
     Returns:
-        None: If the update is successful or if an error occurs during the request.
+    None: If the request is successful or if an exception occurs.
     """
     url = f"http://{host}:{port}/update_function_status"
     
@@ -39,76 +41,50 @@ async def update_function_progress(task_id, category, call_count, last_execution
         "calls_per_second": calls_per_second
     }
     try: 
-        async with aiohttp.ClientSession() as session:  # Create a new session for each request
-            async with session.post(url, json=data) as response: # Send a POST request to the server
-                if response.status == 200:
-                    return None
-    except aiohttp.ClientError as e:
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            return None
+    except requests.RequestException as e:
         return None
-        
+
 class ftrack:
-    """
-    A decorator class to track the execution of functions, including execution time, error count, and call count.
-    Optionally, it can send updates to a web server.
-    Attributes:
-        port (int): The port number for the web server. Default is 5000.
-        host (str): The host address for the web server. Default is "127.0.0.1".
-        taskid (str): An optional task identifier.
-        category (int): An optional category identifier. Default is 0.
-        web (bool): If True, sends updates to a web server. Default is True.
-        command_line (bool): If True, enables command line mode. Default is False.
-        tickrate (int): The rate at which updates are sent to the web server. Default is 1.
-        exec_hist (deque): A deque to store execution history with a maximum length.
-        first_call_time (float): The time when the first call was made.
-        kwargs (dict): Additional keyword arguments.
-        latest_call (float): The time of the latest call.
-        call_count (int): The number of times the decorated function has been called.
-        error_count (int): The number of errors encountered during function execution.
-        file_name (str): The name of the file where the decorator is used.
-    Methods:
-        __call__(func):
-            Decorates the given function to track its execution.
-        async run_update(func, duration):
-            Sends an update to the web server with the function's execution details.
-    """
-    def __init__(self, port=5000, host="127.0.0.1", taskid=None, category=0, web=True, command_line=False, tickrate=1, exec_hist=100, **kwargs):
-        self.port = port
-        self.host = host
-        self.taskid = taskid
-        self.category = category
-        self.web = web
-        self.command_line = command_line
-        self.tickrate = tickrate
-        self.exec_hist = deque(maxlen=exec_hist)
-        self.first_call_time = time.perf_counter()
-        self.kwargs = kwargs
-        self.latest_call = None
-        self.call_count = 0
-        self.error_count = 0
-        self.file_name = os.path.basename(inspect.stack()[1].filename)
-
-    def __call__(self, func):
-        """
-        A decorator that wraps a function to track its execution statistics.
-
-        Args:
-            func (Callable): The function to be wrapped.
-
-        Returns:
-            Callable: The wrapped function with added tracking functionality.
-
-        The wrapper function performs the following tasks:
-            - Increments the call count each time the function is called.
-            - Records the start time of the function execution.
-            - Executes the function and captures any exceptions, incrementing the error count if an exception occurs.
-            - Records the execution duration and appends it to the execution history.
-            - If the web attribute is True and the call count is a multiple of the tickrate, it runs an asynchronous update function.
-        """
-        @wraps(func)
-        def wrapper(*args, **kwargs):
+        def __init__(self, func=None, port=5000, host="127.0.0.1", taskid=None, category=0, web=True, command_line=False, tickrate=1, exec_hist=100, **kwargs):
+            self.func = func
+            self.port = port
+            self.host = host
+            self.taskid = taskid
+            self.category = category
+            self.web = web
+            self.command_line = command_line
+            self.tickrate = tickrate
+            self.exec_hist = deque(maxlen=exec_hist)
+            self.first_call_time = time.perf_counter()
+            self.kwargs = kwargs
+            self.latest_call = None
+            self.call_count = 0
+            self.error_count = 0
+            self.file_name = os.path.basename(inspect.stack()[1].filename)
+            self.lock = threading.Lock()
+            self.initialized = False
+    
+            # If the the first argument is a function, call the decorator with the function as an argument (allows for use as a decorator and a function)
+            if func is not None:
+                if callable(func):
+                    self.func = func
+                    self.__call__(func)
+                else:
+                    raise ValueError("The first argument must be a callable function.")
+            else:
+                self.func = None
+                
+            self.initialized = True
+                
+                
+        def calllogic(self,func, *args, **kwargs):
             self.call_count += 1
             start_time_execution = time.perf_counter_ns()
             try:
+               
                 result = func(*args, **kwargs)
             except Exception as e:
                 self.error_count += 1
@@ -119,31 +95,74 @@ class ftrack:
                 self.exec_hist.append(execution_duration)
             if self.web and self.call_count % self.tickrate == 0:
                 self.latest_call = execution_duration
-                
-                # Schedule the update function as a background task
-                
-                
-                loop = asyncio.get_event_loop()
-                if loop.is_running(): # If the event loop is already running, create a task in order to run the update function in the background
-                    asyncio.create_task(self.run_update(func)) # Run the update function in the background 
-                else: # If the event loop is not running, run it synchronously
-                    loop.run_until_complete(self.run_update(func)) # Run the update function synchronously
+                threading.Thread(
+                    target=self.run_update,
+                    args=(func,),
+                    daemon=True
+                ).start()
             return result
-        return wrapper
+            
+                
+    
+        def __call__(self, *args, **kwargs):
+            
+            if self.func is not None and self.initialized:
+                return self.calllogic(self.func, *args, **kwargs)
+            else:
+                func = args[0]
+                @wraps(func)
+                def wrapper(*args, **kwargs):
+                    return self.calllogic(func, *args, **kwargs)
+                return wrapper
 
-    async def run_update(self, func):
-            asyncio.create_task( update_function_progress(
-                task_id=self.taskid or func.__name__,
-                category=self.category,
-                call_count=self.call_count,
-                last_execution_time=self.latest_call,
-                function_name=func.__name__,
-                exec_hist=list(self.exec_hist),
-                error_count=self.error_count,
-                filename=self.file_name,
-                calls_per_second=self.call_count / (time.perf_counter() - self.first_call_time),
-                host=self.host,
-                port=self.port
-            ))
-
-
+        def run_update(self, func):
+            """
+            Run the update function with a lock to ensure thread safety.
+        
+            This method updates the function progress by calling `update_function_progress` 
+            with various attributes of the object and the provided function.
+        
+            Args:
+                func (function): The function whose progress is being updated.
+            """
+            with self.lock:
+                update_function_progress(
+                    task_id=self.taskid or func.__name__,
+                    category=self.category,
+                    call_count=self.call_count,
+                    last_execution_time=self.latest_call,
+                    function_name=func.__name__,
+                    exec_hist=list(self.exec_hist),
+                    error_count=self.error_count,
+                    filename=self.file_name,
+                    calls_per_second=self.call_count / (time.perf_counter() - self.first_call_time),
+                    host=self.host,
+                    port=self.port
+                )
+    
+        def __getstate__(self):
+            """
+            Prepare the object state for pickling.
+        
+            This method is called when the object is being pickled. It creates a copy of the 
+            object's __dict__ and sets the 'lock' attribute to None because locks cannot be pickled.
+        
+            Returns:
+                dict: The state of the object with 'lock' set to None.
+            """
+            state = self.__dict__.copy()
+            state['lock'] = None  # Locks cannot be pickled
+            return state
+        
+        def __setstate__(self, state):
+            """
+            Restore the object state from the unpickled state.
+        
+            This method is called when the object is being unpickled. It updates the object's 
+            __dict__ with the unpickled state and recreates the 'lock' attribute.
+        
+            Args:
+                state (dict): The unpickled state of the object.
+            """
+            self.__dict__.update(state)
+            self.lock = threading.Lock()  # Recreate the lock after unpickling
